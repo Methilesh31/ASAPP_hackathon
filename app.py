c74ada06ec26d35c343fcf7fecd6a8fe4b26b016
Methilesh31 <methilesh.k31@gmail.com>
2024-10-18 00:17:26 +0530

    app.py  modified

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from rag_system import RAGSystem
 
 
 rag_system = RAGSystem(papers_folder=None)  
-
 st.title("Research Paper QA Bot")
-
 query = st.text_input("Enter your question:")
-
 if query:
     response = rag_system.generate_response(query)
     
